704b921a9cb0b76de1af1a5aa597eb1ea585b320
Sarah Flanery <146489236+sflanery@users.noreply.github.com>
2025-02-25 16:46:44 -0600

    Add files via upload
    Added reverse collision detection

--- ultrasonic_blindspot_detection.py
+++ ultrasonic_blindspot_detection.py
@@ -6,11 +6,17 @@ from tkinter import font  # Import the font module from tkinter for customizing 
 
 from time import sleep  # Import the sleep function from the time module for delay
 
+import threading
+
+# threat checking
+#threat_check = {left, right, back}
+
 # Initialize the ultrasonic sensor
 #Currently using GPIOs 24 and 23 for the right side blind spot, for the second ultrasonic, we will use GPIOs 12 and 16 for the left side blind spot
 
-sensor_right = DistanceSensor(echo=24, trigger=23, max_distance=5)
-sensor_left = DistanceSensor(echo=16, trigger=12, max_distance=5)
+sensor_right = DistanceSensor(echo=24, trigger=23, max_distance=2)
+sensor_left = DistanceSensor(echo=16, trigger=12, max_distance=2)
+sensor_back = DistanceSensor(echo = 17, trigger = 27, max_distance = 2)
 
 # Initialize the Tkinter window
 
@@ -28,39 +34,76 @@ distance_label = tk.Label(window, text="Distance: ", anchor='center', font=custo
 
 distance_label.pack()  # Add the label to the window
 
-def measure_distance():
+def measure_distance_left():
 
 #defining both of the ultrasonic distances to be calculated
-   distance_right = int(sensor_right.distance * 100)  # Measure the distance and convert it to an integer
+    # Measure the distance and convert it to an integer
    distance_left = int(sensor_left.distance * 100)
-
-   #distance_label.config(text="Distance: {} cm".format(distance))  # Update the distance label with the new distance
-
    
 
-   if distance_right < 101:
-
-       distance_label.config(fg="red", text="Right Blind Spot Warning: \nVehicle is {} cm away!\n".format(distance_right))
+   #distance_label.config(text="Distance: {} cm".format(distance))  # Update the distance label with the new distance
 
        # If the distance is less than 1.01 meter, set the label text to display a blindspot detection warning
        # Each if statement is true for both sides
    if distance_left < 101:
+
        distance_label.config(fg = "red", text = "Left Blindspot Warning: \nVehicle is {} cm away\n!".format(distance_left))
-
-   elif distance_right > 101 and distance_left > 101:
-          distance_label.config(fg="blue", text="No Vehicle detected in either blind spot.")
-
-       # If the distance is greater than 1.01 meter, just indicate otherwise
-
        
-
-   window.after(500, measure_distance)  # Schedule the next measurement after 0.5 second
+   #elif distance_right > 101 and distance_left > 101:
+    #      distance_label.config(fg="blue", text="No Vehicle detected in either blind spot.")
 
    
+       # If the distance is greater than 1.01 meter, just indicate otherwise
+ #  elif distance_left > 101:
 
-# Start measuring distance
+     #         distance_label.config(fg = "blue", text = "Nothing detected in right blindspot \n")
+     
+       
+   
+   window.after(500, measure_distance_left)  # Schedule the next measurement after 0.5 second
+  
 
-measure_distance()
+def measure_distance_right():
+   distance_right = int(sensor_right.distance * 100)
+        #copy paste
+   if distance_right < 101:
+            
+              distance_label.config(fg="red", text="Right Blind Spot Warning: \nVehicle is {} cm away!\n".format(distance_right))
+              
+  # elif distance_right > 101:
+    #          distance_label.config(fg = "blue", text = "Nothing detected in left blindspot \n")
+        
+              
+   window.after(500, measure_distance_right)  # Schedule the next measurement after 0.5 second
+   
+
+def measure_distance_back():
+   distance_back = int(sensor_back.distance * 100)
+       
+   if distance_back < 101:
+     
+       distance_label.config(fg = "red", text = "Warning: Object Detected Behind Vehicle {} cm away\n".format(distance_back))
+       
+  # elif distance_back > 101:
+  #     distance_label.config(fg = "blue", text = "Nothing detected behind the vehicle \n")
+      
+   
+   window.after(500, measure_distance_back)  # Schedule the next measurement after 0.5 second
+
+
+              
+
+thread_left = threading.Thread(target = measure_distance_left)
+thread_right = threading.Thread(target = measure_distance_right)
+thread_back = threading.Thread(target = measure_distance_back)
+
+
+
+thread_left.start()
+thread_right.start()
+thread_back.start()
+
+
 
 # Run the Tkinter event loop
 
