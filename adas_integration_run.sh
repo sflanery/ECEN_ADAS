def95bdfc15208aef3f1d9b1182b204125d7edf4
Sarah Flanery <146489236+sflanery@users.noreply.github.com>
2025-09-11 13:32:37 -0500

    Update adas_integration_run.sh
    All subsystems run at the same time - have front end permissions issues. Reboot often due to high demands from pi.

--- adas_integration_run.sh
+++ adas_integration_run.sh
@@ -4,15 +4,14 @@ trap "echo 'Stopping all processes...'; pkill -P $$; exit" SIGINT
 # ----------- BACKEND ----------- #
 echo "Starting backend..."
 cd "$(dirname "$0")"
-source /home/sarsa/dashtest_new/dashtest/backend_server/venv/bin/activate
-python3 /home/sarsa/dastest_new/dashtest/backend_server/app.py &
+source /home/sarsa/dashtest_new/dashtest/backend_server/venv/Scripts/activate
+python3 /home/sarsa/dashtest_new/dashtest/backend_server/app.py &
 deactivate   # leave the venv so next scripts use system Python
 
 # ----------- ADDITIONAL PYTHON SCRIPTS ----------- #
 echo "Starting additional Python scripts..."
-#/usr/bin/python3 /home/sarsa/lane_assist_dashboard.py &
-#/usr/bin/python3 /home/sarsa/ultrasonic_integration_dashboard.py &
-#/usr/bin/python3 /home/sarsa/Downloads/camera.py &
+/usr/bin/python3 /home/sarsa/lane_assist_dashboard.py &
+/usr/bin/python3 /home/sarsa/ultrasonic_integration_dashboard.py &
 /usr/bin/python3 /home/sarsa/camera_models.py
 
 # ----------- FRONTEND ----------- #
@@ -20,5 +19,7 @@ echo "Starting frontend..."
 cd /home/sarsa/dashtest_new/dashtest/car_dashboard
 npm run serve &
 
+
 wait
+
 
